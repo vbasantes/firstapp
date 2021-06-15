620def371dcdf339129543b927d79d324a083120
Victor Basantes <victor_basantes@hotmail.com>
2021-06-15 15:11:01 -0500

    Added buttons and params for get request

--- app/controllers/another_controller.rb
+++ app/controllers/another_controller.rb
@@ -1,6 +1,9 @@
 class AnotherController < ApplicationController
   def index
     @greeting = 'Good morning'
+    @id = params[:id]
+    @pickColor = params[:pickColor]
+    @pickCar = params[:pickSedan]
   end
 
   def update
