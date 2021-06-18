620df2b54be76842eb73071bb76a9433b76d0174
Victor Basantes <victor_basantes@hotmail.com>
2021-06-18 11:00:14 -0500

    Added named scopes to movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ApplicationRecord
+
+  # This is a named scope. It is the preferred way of doing custom queries
+  # Called using: Movie.from_newest.lookup("Modern Times")
+  scope :from_newest,
+    lambda {order("created_at DESC")}
+  scope :lookup,
+    lambda { |titlestring| where (["title LIKE ?", "%#{titlestring}%"])}
 end
